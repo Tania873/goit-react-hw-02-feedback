c60ff03dbf0ec2ad4aa0cc11545a66fa0d409de4
Tania873 <tania.kostyuk01@gmail.com>
2023-02-09 13:20:51 +0200

    hw

---
+++ src/components/App/App.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
+import Statistics from '../Statistics/Statistics';
+import Section from '../Section/Section';
+import Notification from '../Notification';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedbackBtnClick = event => {
+    this.setState(previousState => ({
+      [event.target.innerText.toLowerCase()]:
+        previousState[event.target.innerText.toLowerCase()] + 1,
+    }));
+  };
+
+  render() {
+    const countTotalFeedback = () =>
+      Object.values(this.state).reduce((acc, el) => acc + el, 0);
+
+    const countPositiveFeedbackPercentage = () =>
+      Math.round((100 / countTotalFeedback()) * this.state.good) || 0;
+
+    return (
+      <div className={css.data}>
+        <div className={css.box}>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={this.state}
+              onLeaveFeedback={this.handleFeedbackBtnClick}
+            />
+          </Section>
+          <Section title="Statistics">
+            {countTotalFeedback() === 0 ? 
+              <Notification message="There is no feedback" /> :
+              <Statistics
+                good={this.state.good}
+                neutral={this.state.neutral}
+                bad={this.state.bad}
+                total={countTotalFeedback()}
+                positivePercentage={countPositiveFeedbackPercentage()}
+              />}
+          </Section>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div className={css.buttonBox}>
+      {Object.keys(options).map(el => (
+        <button type="button" className={css.button} key={el} onClick={onLeaveFeedback}>
+          {el}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
